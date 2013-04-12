ca6e8f9842268ae6e935f3d0c874fd2fcab16474
See Guo Lin <see@guol.in>
2013-04-12 20:46:06 +0800

    Fixed bug on localStorage printing issue.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,6 +1,7 @@
 for (var i = 0; i < localStorage.length; i++){
-  if(localStorage.key(i) !== "ticket" || localStorage.key(i) !== "token" || localStorage.key(i) !== "uuid"){
-    $('.status').append(localStorage.key(i));
-    $('.status').append(localStorage.getItem(localStorage.key(i)));
+  if(localStorage.key(i) == "ticket" || localStorage.key(i) == "token" || localStorage.key(i) == "uuid"){
+  	continue;
   }
+  $('.status').append(localStorage.key(i));
+  $('.status').append(localStorage.getItem(localStorage.key(i)));
 }
